ccda4e98aed3b4c0c9778cad76dbd97a55fce2e2
Aadarsh Kc <aadarshkc2061@gmail.com>
2025-06-05 08:54:30 +0545

    encryption, decryption

---
+++ src/controller/globals/authController.ts
@@ -0,0 +1,26 @@
+import {Request,Response} from "express"
+
+// const registerUser = (req:Request,res:Response)=>{
+    
+// }
+
+class AuthController{
+    registerUser(req:Request,res:Response){
+
+    }
+}
+
+exports.registerUser = ()=>{
+
+}
+
+exports.loginUser = ()=>{
+    static async registerUser(){
+
+    }
+    static async loginUser(){
+
+    }
+}
+
+module.exports = AuthController
