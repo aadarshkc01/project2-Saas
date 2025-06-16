c562f606bedccfdb09e1fe55c8aa0540107158a2
Aadarsh Kc <aadarshkc2061@gmail.com>
2025-06-16 23:39:30 +0545

    Middleare

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken"
+import User from "../database/models/userModel";
 
 class Middleware {
     static isLoggedIn(req:Request,res:Response,next:NextFunction){
@@ -13,7 +14,7 @@ class Middleware {
         }
 
         //verify garne
-        jwt.verify(token, 'thisissecretehai', (erroraayo,resultaayo)=>{
+        jwt.verify(token, 'thisissecretehai', async(erroraayo,resultaayo:any)=>{
             if (erroraayo) {
                 res.status(403).json({
                     message: "Token invalid vayo"
@@ -21,9 +22,21 @@ class Middleware {
             } else {
                 //verified vayo
                 console.log(resultaayo, "Result aayo")
+                const userData = await User.findAll({
+                    where:{
+                        id : resultaayo.id
+                    }
+                })
+                if (userData.length === 0){
+                    res.status(404).json({
+                        message : "No user with that id, invalid token"
+                    })
+                }else{
+                    console.log("Successfully verified")
+                }
             }
         } )
-
+        next()
     }
 }
 
